da04fa23fe5ad32030693abd72dadd17012a8e08
aadisirurmath <aadisirurmath@gmail.com>
2024-11-24 12:16:08 +0530

    Update comb1_tb.sv

--- orange/comb1/comb1_tb.sv
+++ orange/comb1/comb1_tb.sv
@@ -9,11 +9,11 @@ comb1 uut (
     .Y(Y)
   );
   initial begin
-    A = 0; B = 0; C = 0; D = 0;
+    A = 0; B = 1; C = 0; D = 1;
     #10;
-    A = 1; B = 1; C = 1; D = 1;
+    A = 1; B = 1; C = 0; D = 1;
     #10;
-    A = 1; B = 1; C = 1; D = 1;
+    A = 1; B = 0; C = 1; D = 0;
     #10;
     $stop;
     end
